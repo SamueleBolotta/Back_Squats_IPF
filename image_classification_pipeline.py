d2d81d34a48d3c919aa55a9e509e01908b8ae4bc
SamueleBolotta <bolottasamuele@gmail.com>
2024-01-25 14:46:22 +0100

    iter

--- image_classification_pipeline.py
+++ image_classification_pipeline.py
@@ -4,34 +4,38 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import models
 from torchvision import transforms
-from utils.utils import CustomImageDataset
+from utils.utils import CustomImageDataset, build_rgb_dataset, save_images
 from torch.utils.data import DataLoader, random_split
 from utils.utils import custom_collate, plot_losses, visualize_transformations
 import os
 from PIL import Image
-from torchvision import transforms
-import numpy as np
 import pandas as pd
 from torchvision import datasets
+from tqdm import tqdm
 
 # Define transformations
 transform = transforms.Compose([
     transforms.Resize((224, 224)),  # Resize images to 224x224
-    transforms.RandomPerspective(distortion_scale=0.2, p=0.5, fill=0),  # Apply random perspective transformation
-    transforms.RandomGrayscale(p=0.1),  # Convert images to grayscale with a probability of 0.1
-    transforms.ColorJitter(brightness=0.2, contrast=0.2),  # Randomly change the brightness and contrast
-    # transforms.ToTensor(),  # Convert the image to PyTorch Tensor data type
+    transforms.RandomPerspective(distortion_scale=0.2, p=0.5, fill=0),
+    transforms.RandomGrayscale(p=0.1),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalization
 ])
 
 image_labels = []
-# get the current working directory
-current_dir = os.path.abspath(os.getcwd())
+current_dir = os.path.abspath(os.getcwd()) # current working directory
+original_dataset = f"{current_dir}/dataset"
+train, test = build_rgb_dataset(original_dataset, train_ratio=0.8)
+save_images(train, "train") # saved to path f"{current_dir}/dataset/{sex}/train"
+save_images(test, "test") # saved to path f"{current_dir}/dataset/{sex}/test"
 
+# create new augmented dataset saved to f"{current_dir}/dataset/{sex}/train_augmented"
 for sex in ("Men", "Women"):
     # Path to your original dataset
-    original_dataset_path = f'{current_dir}/dataset/{sex}/Original'
+    original_dataset_path = f'{current_dir}/dataset/{sex}/train'
     # Path to save augmented images
-    augmented_dataset_path = f'{current_dir}/dataset/{sex}/Augmented'
+    augmented_dataset_path = f'{current_dir}/dataset/{sex}/train_augmented'
 
     if not os.path.exists(augmented_dataset_path):
         os.makedirs(augmented_dataset_path)
@@ -39,18 +43,17 @@ for sex in ("Men", "Women"):
     # Traverse the directory structure
     ## Pay attention to the fact that the images are in format RGBA, which is not supported by JPEG
     ## Convert it to RGB before saving
-
     for root, dirs, files in os.walk(original_dataset_path):
         # The subfolder name is the label
         label = os.path.basename(root)
 
-        for file in files:
+        for file in tqdm(files):
             if file.endswith('.jpg') or file.endswith('.png'):
                 # avoid using images that are unlabelled
                 if not "Screenshot" in file:
                     file_path = os.path.join(root, file)
                     image = Image.open(file_path)
-                    
+
                     # Convert image to RGB if it has more than 3 channels
                     if image.mode != 'RGB':
                         image = image.convert('RGB')
@@ -60,28 +63,10 @@ for sex in ("Men", "Women"):
                     for i in range(5):
                         if not os.path.exists(path_with_label):
                             os.makedirs(path_with_label)
-                        transformed_image = transform(image)
-                        # transformed_image = transforms.ToPILImage()(transformed_image)
-                        transformed_image.save(os.path.join(path_with_label, f'{i}_{file}'))
-    # Create CSV file
-    # Path to your dataset (including augmented images)
-    dataset_path = '/home/samuele/Documenti/GitHub/Back_Squats_IPF/dataset'
-
-    # Traverse the directory structure
-    for root, dirs, files in os.walk(dataset_path):
-        label = os.path.basename(root)
-        for file in files:
-            if file.endswith('.jpg') or file.endswith('.png'):
-                # avoid using images that are unlabelled
-                if not "Screenshot" in file:
-                    # Get the paths from root
-                    path_from_root = os.path.relpath(os.path.join(root, file), dataset_path)
-                    # print(root)
-                    # print(path_from_root)
-                    # Add the file and label to the list
-                    image_labels.append((file, label, path_from_root))
-
-df = pd.DataFrame(image_labels, columns=["image", "label", "relative path"])
+                            
+                        # Copy the original image to the augmented dataset
+                        original_img_dest = os.path.join(path_with_label, file)
+                        shutil.copy2(file_path, original_img_dest)
 
 new_labels = {'Frontal_Above_parallel':'above', 'Lateral_Above_parallel':'above',
               'Frontal_Parallel':'parallel', 'Lateral_Parallel':'parallel',
@@ -91,14 +76,9 @@ new_labels = {'Frontal_Above_parallel':'above', 'Lateral_Above_parallel':'above'
               'Frontal - Valid':'valid', 'Lateral - Valid':'valid'}
 
 # Save the DataFrame to a CSV file
-df['label'] = df['label'].replace(new_labels)
-df.to_csv('image_labels.csv', index=False)
-
-# Create a dataset using the ImageFolder class
-dataset = datasets.ImageFolder(root=f'{current_dir}/dataset', transform=transform)
-
-# get the current working directory
-current_dir = os.getcwd()
+train_df = pd.DataFrame(image_labels, columns=["image", "label", "relative path"])
+train_df['label'] = train_df['label'].replace(new_labels)
+train_df.to_csv('image_labels.csv', index=False)
 
 # Path to your dataset
 dataset_path = f'{current_dir}/dataset/'
@@ -114,13 +94,11 @@ random_seed = 42
 
 # Create the training and validation datasets
 generator1 = torch.Generator().manual_seed(42)
-train_dataset, val_dataset, test_dataset = random_split(dataset, [0.8, 0.1, 0.1], generator=generator1)
+train_dataset, val_dataset = random_split(dataset, [0.9, 0.1], generator=generator1)
 
 # Load the training and validation data in batches 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=custom_collate)
-
 # Set the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -259,13 +237,50 @@ for epoch in range(n_epochs):
 
 # Set the model to evaluation mode
 model.eval()
-                                                                                                                           
+                                                                                                      
 # Initialize the running loss and correct predictions
 running_loss = 0.0
 correct_predictions = 0.0
-                                                                                                                           
-# Loop over the test data loader
 
+# preprocess the test dataset for evaluation
+print("PREPROCESSING TEST DATASET FOR EVALUATION...")
+test_image_labels = []
+for sex in ("Men", "Women"):
+    # Path to your original dataset
+    original_dataset_path = f'{current_dir}/dataset/{sex}/test'
+
+    # Traverse the directory structure
+    ## Pay attention to the fact that the images are in format RGBA, which is not supported by JPEG
+    ## Convert it to RGB before saving
+    for root, dirs, files in os.walk(original_dataset_path):
+        # The subfolder name is the label
+        label = os.path.basename(root)
+
+        for file in tqdm(files):
+            if file.endswith('.jpg') or file.endswith('.png'):
+                # avoid using images that are unlabelled
+                if not "Screenshot" in file:
+                    file_path = os.path.join(root, file)
+                    relative_path = os.path.relpath(file_path, f'{current_dir}/dataset/')
+                    test_image_labels.append((file, label, relative_path))
+
+# Save the DataFrame to a CSV file
+df_test = pd.DataFrame(test_image_labels, columns=["image", "label", "relative path"])
+df_test['label'] = df_test['label'].replace(new_labels)
+df_test.to_csv('test_image_labels.csv', index=False)
+df_test_file = f'{current_dir}/test_image_labels.csv'
+
+# Define transformations for testing (no random transformations)
+test_transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # Resize images to 224x224
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+test_dataset = CustomImageDataset(df_test_file, dataset_path, transform=test_transform)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=custom_collate)
+
+# Loop over the test data loader
 for data, target in test_loader:
     # Transfer the data to the GPU
     data = data.to(device)
